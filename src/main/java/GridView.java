efdaeb5c2f3618857bce0b489c7d04d3d8d00c1d
Deepansh Sachdeva <deepansh.99530@gmail.com>
2020-11-29 17:10:38 -0500

    :eyes: params panel

--- src/main/java/GridView.java
+++ src/main/java/GridView.java
@@ -1,5 +1,8 @@
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.basic.BasicButtonListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,17 +28,21 @@ public class GridView implements ActionListener {
         frame.setSize(frameWidth, frameHeight);
         frame.setBackground(Color.GREEN);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.add(getNorthPanel(), BorderLayout.NORTH);
+
         panel = new MyPanel();
-        frame.add(panel, BorderLayout.CENTER);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        splitPane.setLeftComponent(getParamsPanel());
+        splitPane.setDividerLocation(300);
+        splitPane.setRightComponent(panel);
+
+        frame.add(splitPane);
 
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
         new GridView();
-
     }
 
 
@@ -57,13 +64,118 @@ public class GridView implements ActionListener {
     }
 
 
-    private JPanel getNorthPanel() {
+    private JPanel getParamsPanel() {
         btnStart = new JButton("Start");
         btnStart.setActionCommand("Start");
         btnStart.addActionListener(this);
-        JPanel pan = new JPanel();
-        pan.add(btnStart);
-        return pan;
+
+        JPanel paramsPanel = new JPanel();
+
+        paramsPanel.setLayout(new BoxLayout(paramsPanel, BoxLayout.Y_AXIS));
+
+        /**
+         * Population
+         *
+         */
+        paramsPanel.add(new Label("Population:"));
+        JSlider populationSlider = new JSlider(100, 300, 200);
+        populationSlider.setMinorTickSpacing(10);
+        populationSlider.setMajorTickSpacing(50);
+        populationSlider.setPaintTicks(true);
+        populationSlider.setPaintLabels(true);
+
+        final Label populationLabel = new Label();
+        populationLabel.setText(String.valueOf(populationSlider.getValue()));
+
+        populationSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider jslider = (JSlider) e.getSource();
+                populationLabel.setText(String.valueOf(jslider.getValue()));
+            }
+        });
+
+        paramsPanel.add(populationSlider);
+        paramsPanel.add(populationLabel);
+
+        /**
+         * Infected Population
+         *
+         */
+        paramsPanel.add(new Label("Infected Population:"));
+        JSlider infectedSlider = new JSlider(0, 100, 5);
+        infectedSlider.setMinorTickSpacing(5);
+        infectedSlider.setMajorTickSpacing(10);
+        infectedSlider.setPaintTicks(true);
+        infectedSlider.setPaintLabels(true);
+
+        final Label infectedLabel = new Label();
+        infectedLabel.setText(String.valueOf(infectedSlider.getValue()));
+
+        infectedSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider jslider = (JSlider) e.getSource();
+                infectedLabel.setText(String.valueOf(jslider.getValue()));
+            }
+        });
+
+        paramsPanel.add(infectedSlider);
+        paramsPanel.add(infectedLabel);
+
+        /**
+         * Social Distance
+         *
+         */
+        paramsPanel.add(new Label("Social Distance:"));
+        JSlider socialDistanceSlider = new JSlider(0, 100, 5);
+        socialDistanceSlider.setMinorTickSpacing(5);
+        socialDistanceSlider.setMajorTickSpacing(10);
+        socialDistanceSlider.setPaintTicks(true);
+        socialDistanceSlider.setPaintLabels(true);
+
+        final Label socialDistanceLabel = new Label();
+        socialDistanceLabel.setText(String.valueOf(infectedSlider.getValue()));
+
+        socialDistanceSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider jslider = (JSlider) e.getSource();
+                socialDistanceLabel.setText(String.valueOf(jslider.getValue()));
+            }
+        });
+
+        paramsPanel.add(socialDistanceSlider);
+        paramsPanel.add(socialDistanceLabel);
+
+        /**
+         * Death Rate
+         *
+         */
+        paramsPanel.add(new Label("Death Rate:"));
+        JSlider deathSlider = new JSlider(0, 100, 5);
+        deathSlider.setMinorTickSpacing(5);
+        deathSlider.setMajorTickSpacing(10);
+        deathSlider.setPaintTicks(true);
+        deathSlider.setPaintLabels(true);
+
+        final Label deathLabel = new Label();
+        deathLabel.setText(String.valueOf(infectedSlider.getValue()));
+
+        socialDistanceSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider jslider = (JSlider) e.getSource();
+                deathLabel.setText(String.valueOf(jslider.getValue()));
+            }
+        });
+
+        paramsPanel.add(deathSlider);
+        paramsPanel.add(deathLabel);
+
+        paramsPanel.add(btnStart);
+
+        return paramsPanel;
     }
 
 }
