e89d1ca549cc84ed8fe0d7a421ddd5a265b3723f
Deepansh Sachdeva <deepansh.99530@gmail.com>
2020-12-06 17:52:32 -0500

    :zap: configure unit test workflow

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -5,6 +5,10 @@ public class UnitTest {
 
     @Test
     void demoTest(){
+        List<Integer> list = new ArrayList<>();
+        list.stream().forEach(x -> {
+            System.out.println(x);
+        });
         Assertions.assertEquals(1,1);
     }
 }
